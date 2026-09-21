9411bf6514d3c4243f87fdc842bf67abeaa31e50
ParthDani17 <parthdani17@gmail.com>
2026-09-21 10:00:36 +0530

    Updated App component to display Home page on root route

--- src/skillbridge-frontend/src/App.jsx
+++ src/skillbridge-frontend/src/App.jsx
@@ -1,18 +1,34 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+
 function App() {
     return (
         <BrowserRouter>
+
+            <Navbar />
+
             <Routes>
-                <Route path="/" element={<h1>SkillBridge</h1>} />
+
+                <Route
+                    path="/"
+                    element={<Home />}
+                />
+
                 <Route
                     path="/login"
                     element={<h1>Login</h1>}
                 />
+
                 <Route
                     path="/register"
                     element={<h1>Register</h1>}
                 />
+
             </Routes>
+
         </BrowserRouter>
     );
 }
