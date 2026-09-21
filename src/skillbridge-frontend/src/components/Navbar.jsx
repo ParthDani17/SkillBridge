e1fde1eaa149c33208875bb52a7af5f9b8bce0e3
ParthDani17 <parthdani17@gmail.com>
2026-09-21 09:51:01 +0530

    Added Navbar component

---
+++ src/skillbridge-frontend/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+
+            <div className="navbar-logo">
+                <Link to="/">
+                    SkillBridge
+                </Link>
+            </div>
+
+            <div className="navbar-links">
+
+                <Link to="/">
+                    Home
+                </Link>
+
+                <Link to="/login">
+                    Login
+                </Link>
+
+                <Link to="/register">
+                    Register
+                </Link>
+
+            </div>
+
+        </nav>
+    );
+}
+
+export default Navbar;
