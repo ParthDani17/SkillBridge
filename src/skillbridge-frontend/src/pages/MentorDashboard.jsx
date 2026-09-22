2f373c682dd2167e2699e966ef846c1f63601b4a
ParthDani17 <parthdani17@gmail.com>
2026-09-23 00:47:52 +0530

    Added login, admin, mentor, and student dashboard components with styles

---
+++ src/skillbridge-frontend/src/pages/MentorDashboard.jsx
@@ -0,0 +1,10 @@
+function MentorDashboard() {
+    return (
+        <div>
+            <h1>Mentor Dashboard</h1>
+            <p>Welcome, Mentor!</p>
+        </div>
+    );
+}
+
+export default MentorDashboard;
