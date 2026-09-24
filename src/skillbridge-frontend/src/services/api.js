0671e030b11677b8a5fbc3e7e93af3a504588a8e
ParthDani17 <parthdani17@gmail.com>
2026-09-25 01:29:58 +0530

    Added request interceptor to api for token management

--- src/skillbridge-frontend/src/services/api.js
+++ src/skillbridge-frontend/src/services/api.js
@@ -4,4 +4,20 @@ const api = axios.create({
     baseURL: "http://localhost:5000/api/v1"
 });
 
+api.interceptors.request.use(
+    (config) => {
+
+        const token = localStorage.getItem("accessToken");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
 export default api;
